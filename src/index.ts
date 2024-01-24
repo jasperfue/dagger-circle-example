35d783c4b8738a303fefe5ac7b82057003a03248
Jasper Fülle <93281533+jasperfue@users.noreply.github.com>
2024-01-24 14:12:11 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ import { calculateCircleArea } from './circle';
 const radius = 5;
 const area = calculateCircleArea(radius);
 console.log(`The area of the circle with a radius of ${radius} is ${area}.`);
+console.log('')
